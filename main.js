458bb06b48275b594fd5a8185205f08f6cac2384
Takashi Idobe <idobetakashi@gmail.com>
2018-05-16 10:22:06 -0500

    Portfolio Site
    Initial Commit

---
+++ main.js
@@ -0,0 +1,6 @@
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sideNav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+$(".button-collapse").sideNav();
+
